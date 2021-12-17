e5d360f12e0005e31658ae42d47da62ac7816d78
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-12-17 17:52:05 +0530

    Added: Display message in web file is added

--- src/main/java/com/example/demo/controller/HelloWebController.java
+++ src/main/java/com/example/demo/controller/HelloWebController.java
@@ -6,11 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWebController {
+	
+	/**
+	 * Call method to display message in Web
+	 * @return
+	 */
 	@GetMapping("/web")
 	public String hello() {
 		return "hello";
 	}
 	
+	/**
+	 * Call method to display message in Web
+	 * @return
+	 */
 	@GetMapping("/web/message")
 	public String message(Model model) {
 		model.addAttribute("message","This is a custom message.Hello from bridgelabz");
